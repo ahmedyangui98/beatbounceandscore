66d64cf67aa714a059ec7f8e320dc64988b583df
baha <Bahaeddine.tlili@esprit.tn>
2023-04-12 01:38:23 +0100

    Crud backend fixed

--- backend/controlles/quizcontroller.js
+++ backend/controlles/quizcontroller.js
@@ -6,7 +6,7 @@ const Results = require("../model/resultSchema");
 const users = require("../model/user");
 
 
-
+/*here*/
 
 
 /** get all questions */
@@ -20,8 +20,8 @@ exports.getQuestions = async (req, res) => {
 }
 exports.getByTypesQuestions = async (req, res) => {
     try {
-      /*  const q = await Questions.find({type: req.body.type});*/ 
-      const q = await Questions.find({type:req.params.type})
+        /*  const q = await Questions.find({type: req.body.type});*/
+        const q = await Questions.find({ type: req.params.type })
 
         res.json(q)
     } catch (error) {
@@ -30,11 +30,11 @@ exports.getByTypesQuestions = async (req, res) => {
 }
 
 /** insert all questinos */
-exports.insertQuestions = async (req, res) =>{
-    const {questions,answers} = req.body;
+exports.insertQuestions = async (req, res) => {
+    const { questions, answers } = req.body;
     try {
-        Questions.insertMany(req.body , function(err, data){
-            res.json({ msg: "Data Saved Successfully...!",data})
+        Questions.insertMany(req.body, function (err, data) {
+            res.json({ msg: "Data Saved Successfully...!", data })
         })
     } catch (error) {
         res.json({ error })
@@ -42,13 +42,13 @@ exports.insertQuestions = async (req, res) =>{
 }
 
 /** Delete all Questions */
-exports.dropQuestions = async (req, res) =>{
-   try {
+exports.dropQuestions = async (req, res) => {
+    try {
         await Questions.deleteMany();
-        res.json({ msg: "Questions Deleted Successfully...!"});
-   } catch (error) {
+        res.json({ msg: "Questions Deleted Successfully...!" });
+    } catch (error) {
         res.json({ error })
-   }
+    }
 }
 
 /** get all result */
@@ -63,7 +63,7 @@ exports.getResult = async (req, res) => {
 
 exports.getResultByID = async (req, res) => {
     try {
-        const r = await Results.find({username:req.params.id});
+        const r = await Results.find({ username: req.params.id });
         res.json(r)
     } catch (error) {
         res.json({ error })
@@ -72,7 +72,7 @@ exports.getResultByID = async (req, res) => {
 
 exports.getResultByIdResult = async (req, res) => {
     try {
-        const result = await Results.findOne({_id:req.params.id});
+        const result = await Results.findOne({ _id: req.params.id });
         //res.json(r)
         res.status(200).send({ msg: "result", result });
 
@@ -84,29 +84,29 @@ exports.getResultByIdResult = async (req, res) => {
 
 /** post all result */
 exports.storeResult = async (req, res) => {
-   try {
-        const { username, result, attempts, points, achived ,type } = req.body;
-        if(!username && !result) throw new Error('Data Not Provided...!');
+    try {
+        const { username, result, attempts, points, achived, type } = req.body;
+        if (!username && !result) throw new Error('Data Not Provided...!');
         const user = await users.findById(req.body.username);
-        Results.create({ username, result, attempts, points, achived ,type}, function(err, data){
-           // console.log(data._id)
+        Results.create({ username, result, attempts, points, achived, type }, function (err, data) {
+            // console.log(data._id)
             user.result.push(data._id);
             user.save();
 
-            res.json({ msg : "Result Saved Successfully...!"})
+            res.json({ msg: "Result Saved Successfully...!" })
 
         })
 
-   } catch (error) {
-        res.json({error})
-   }
+    } catch (error) {
+        res.json({ error })
+    }
 }
 
 /** delete all result */
 exports.dropResult = async (req, res) => {
     try {
         await Results.deleteMany();
-        res.json({ msg : "Result Deleted Successfully...!"})
+        res.json({ msg: "Result Deleted Successfully...!" })
     } catch (error) {
         res.json({ error })
     }
